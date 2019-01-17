3e13f96e92cbe9b84b11da5c77b315973dce0d38
fabi030 <46783639+fabi030@users.noreply.github.com>
2019-01-17 14:55:52 +0100

    Update parameters.h

--- src/modules/sensors/parameters.h
+++ src/modules/sensors/parameters.h
@@ -150,6 +150,7 @@ struct Parameters {
 	int32_t air_cmodel;
 	float air_tube_length;
 	float air_tube_diameter_mm;
+	float dp_gain;
 };
 
 struct ParameterHandles {
@@ -235,6 +236,7 @@ struct ParameterHandles {
 	param_t air_cmodel;
 	param_t air_tube_length;
 	param_t air_tube_diameter_mm;
+	param_t dp_gain;
 
 };
 
